cf621b69473b8876db576a0ec6e502ec2c4aa346
Tristan L <tristan97little@gmail.com>
2022-04-23 16:07:49 -0600

    Added helpfulness and report routes. Updated get reviews to filter reported

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ app.get('/reviews/meta', (req, res, next) => {
   })
 })
 
-
 app.get('/reviews', (req, res, next) => {
   var product_id = req.query.product_id;
   var page = req.query.page || 0;
@@ -48,7 +47,7 @@ app.get('/reviews', (req, res, next) => {
   db.query(`SELECT JSON_BUILD_OBJECT(
     'results', (SELECT JSON_AGG(ROW_TO_JSON(reviews)) FROM (SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness,
       (SELECT JSON_AGG(ROW_TO_JSON(photos)) photos FROM (SELECT url FROM photos WHERE review_id = reviews.review_id) photos)
-      FROM reviews WHERE product_id = $1 LIMIT $2 OFFSET $3) reviews)
+      FROM reviews WHERE product_id = $1 AND report = 'f' LIMIT $2 OFFSET $3) reviews)
       ) object`, [product_id, count, page], (err, result) => {
         if (err) {
           return next(err);
@@ -63,6 +62,24 @@ app.get('/reviews', (req, res, next) => {
         return result;
       }
     )
+})
+
+app.put(/reviews/:review_id/helpful, (req,res,next) => {
+  db.query(`UPDATE reviews SET helpfulness = helpfulness + 1 WHERE review_id = $1`, [req.params.review_id], (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.end;
+  })
+})
+
+app.put(/reviews/:review_id/report, (req,res,next) => {
+  db.query(`UPDATE reviews SET helpfulness = 't' WHERE review_id = $1`, [req.params.review_id], (err, result) => {
+    if (err) {
+      return next(err);
+    }
+    res.end;
+  })
 })
 
 app.listen(PORT, () => {
