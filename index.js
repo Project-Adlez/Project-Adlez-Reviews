ba937184d0f141df921e07e43d49a65a8d48251e
Tristan L <tristan97little@gmail.com>
2022-04-27 12:41:50 -0600

    Optimizations

--- index.js
+++ index.js
@@ -10,8 +10,11 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 app.get('/reviews/meta', (req, res, next) => {
+
   var product_id = req.query.product_id;
+
   console.log('Metadata request received for product #', product_id);
+  var start = Date.now();
   db.query(`SELECT JSON_BUILD_OBJECT(
     'product_id',
     (SELECT product_id FROM reviews WHERE product_id = $1 LIMIT 1),
@@ -35,6 +38,7 @@ app.get('/reviews/meta', (req, res, next) => {
       return next(err);
     }
     console.log('Meta Data Request Finished');
+    console.log(Date.now() - start, 'ms');
     res.send(result.rows[0].meta);
     return result;
   })
@@ -87,36 +91,3 @@ app.put('/reviews/:review_id/report', (req,res,next) => {
 app.listen(PORT, () => {
   console.log(`The SDC is running on: http://localhost:${PORT}.`);
 });
-
-//Reviews
-
-
-/*
-//Main Get
-SELECT JSON_BUILD_OBJECT(
-  'results', (SELECT JSON_AGG(ROW_TO_JSON(reviews))
-  FROM (SELECT review_id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness,
-    (SELECT JSON_AGG(ROW_TO_JSON(photos)) photos FROM (SELECT url FROM photos WHERE review_id = 5) photos)
-    FROM reviews WHERE product_id = $1) reviews)
-    ) object
-
-//Meta Get
-Rating
-
-SELECT JSON_BUILD_OBJECT(
-  'ratings', (SELECT JSON_BUILD_OBJECT(
-    '1', (SELECT COUNT(review_id) FROM reviews WHERE review_id = $1 AND rating = 1),
-    '2', (SELECT COUNT(review_id) FROM reviews WHERE review_id = $1 AND rating = 2),
-    '3', (SELECT COUNT(review_id) FROM reviews WHERE review_id = $1 AND rating = 3),
-    '4', (SELECT COUNT(review_id) FROM reviews WHERE review_id = $1 AND rating = 4),
-    '5', (SELECT COUNT(review_id) FROM reviews WHERE review_id = $1 AND rating = 5)
-  )),
-  'recommended', (SELECT JSON_BUILD_OBJECT(
-    'false', (SELECT COUNT(review_id) FROM reviews WHERE review_id = $1 AND recommend = f),
-    'true', (SELECT COUNT(review_id) FROM reviews WHERE review_id = $1 AND recommend = t),
-  )),
-  'characteristics', (SELECT JSON_AGG(ROW_TO_JSON(meta)) FROM (SELECT id, name FROM characteristics WHERE product_id = $1, (SELECT AVG(value) FROM characteristics_review WHERE characteristic_id = characteristics.id)) meta)
-)
-
-
-*/
--- test/test.js
+++ test/test.js
@@ -4,5 +4,5 @@ import{sleep} from 'k6';
 export default function () {
   http.get('http://127.0.0.1:3000/reviews/?product_id=65633');
   http.get('http://127.0.0.1:3000/reviews/meta/?product_id=65633')
-  sleep(1);
+  sleep(0.1);
 }
